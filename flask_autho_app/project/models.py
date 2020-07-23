fd4db4cc0a448d49e5c7d447d6fd859dab5b4e5b
Anais <a6anaistang@hotmail.co.uk>
2020-07-23 19:45:28 +0100

    initialised db with python on terminal

---
+++ flask_autho_app/project/models.py
@@ -0,0 +1,8 @@
+from . import db
+
+# python code to create a user model with columns for an id, email, password, name
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
