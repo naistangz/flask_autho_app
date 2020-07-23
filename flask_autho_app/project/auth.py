bf4893523d10d3f3168a8b1f1f630e9dd6d89594
Anais <a6anaistang@hotmail.co.uk>
2020-07-23 20:00:19 +0100

    added flash messages sign up html

--- flask_autho_app/project/auth.py
+++ flask_autho_app/project/auth.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
 
 auth = Blueprint('auth', __name__)
@@ -10,6 +11,29 @@ def login():
 @auth.route('/signup')
 def signup():
     return render_template('signup.html')
+
+@auth.route('/signup', methods=['POST'])
+def signup_post():
+    email = request.form.get('email')
+    name = request.form.get('name')
+    password = request.form.get('password')
+
+    user = User.query.filter_by(email=email).first() # if this returns a user, then the email already exists in database
+
+    # if a user is found, we want to redirect back to signup page so user can try again
+    if user:
+        flash('Email address already exists')
+        return redirect(url_for('auth.signup'))
+
+    # create a new user with the form data. Hash the password so the plaintext version isn't saved.
+    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+
+    # add the new user to the database
+    db.session.add(new_user)
+    db.session.commit()
+    # code to validate and add user to database goes here
+    return redirect(url_for('auth.login'))
+
 
 @auth.route('/logout')
 def logout():
