0abdd56aaecdc245a51e688f2abb701dc13a5123
Anais <a6anaistang@hotmail.co.uk>
2020-07-23 19:14:02 +0100

    auth updated

--- flask_autho_app/project/auth.py
+++ flask_autho_app/project/auth.py
@@ -1,45 +1,15 @@
-from flask import Blueprint, render_template, redirect, url_for, request, flash
-from flask_login import login_user
-from werkzeug.security import generate_password_hash, check_password_hash
-from .models import User
+from flask import Blueprint
 from . import db
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return render_template('login.html')
+    return 'Login'
 
 @auth.route('/signup')
 def signup():
-    return render_template('signup.html')
-
-# creating a new user in the account
-@auth.route('/signup', methods=['POST'])
-def signup_post():
-    # if a user is found, we want to redirect back to signup page so user can try again
-    if user:
-        flash('Email address already exists')
-        return redirect(url_for('auth.signup'))
-
-@auth.route('/login', methods=['POST'])
-def login_post():
-    email = request.form.get('email')
-    password = request.form.get('password')
-    remember = True if request.form.get('remember') else False
-
-    user = User.query.filter_by(email=email).first()
-
-    # check if the user actually exists
-    # take the user-supplied password, hash it, and compare it to the hashed password in the database
-    if not user or not check_password_hash(user.password, password):
-        flash('Please check your login details and try again.')
-        return redirect(url_for('auth.login'))  # if the user doesn't exist or password is wrong, reload the page
-
-    # if the above check passes, then we know the user has the right credentials
-    # adding login_user function before we redirect to the profile page to create the session
-    login_user(user, remember=remember)
-    return redirect(url_for('main.profile'))
+    return 'Signup'
 
 @auth.route('/logout')
 def logout():
--- flask_autho_app/project/main.py
+++ flask_autho_app/project/main.py
@@ -1,12 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint
 from . import db
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    return 'Index'
 
 @main.route('/profile')
 def profile():
-    return render_template('profile.html')
+    return 'Profile'
